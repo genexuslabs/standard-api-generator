ef3c90badec1a337974b9c8724f7f2360992e682
Marcos Crispino <mcrispino@genexus.com>
2019-03-19 16:04:14 -0300

    Added some tests

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -1,24 +1,21 @@
 const generator = require("../generator");
 const options = require("./options.json");
-const metadata = require("./metadata.json");
+const fs = require("fs");
 
 describe("simple function generation", () => {
+  var metadata = require("./std_func.json");
+  var expected = fs.readFileSync("test/std_func.out.ts", "utf8");
   const result = generator(metadata, options);
   it(`should work`, () => {
-    expect(result).toBe(`
-function notImplemented() {
-  console.log("Not yet implemented");
-}
+    expect(result).toEqual(expected);
+  });
+});
 
-/**
-*
-* @param Value
-* @param Months
-*/
-export function AddMth(Value: Date, Months: number) {
-  notImplemented();
-};
-
-`);
+describe("standard type method generation", () => {
+  var metadata = require("./std_type_method.json");
+  var expected = fs.readFileSync("test/std_type_method.out.ts", "utf8");
+  const result = generator(metadata, options);
+  it(`should work`, () => {
+    expect(result).toEqual(expected);
   });
 });
