e18b66d60da64a3e0e7e6adc3f41200edc2513a8
Marcos Crispino <mcrispino@genexus.com>
2019-03-19 16:20:40 -0300

    Test cases refactoring

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -2,29 +2,28 @@ const generator = require("../generator");
 const options = require("./options.json");
 const fs = require("fs");
 
-describe("simple function generation", () => {
-  var metadata = require("./std_func.json");
-  var expected = fs.readFileSync("test/std_func.out.ts", "utf8");
-  const result = generator(metadata, options);
-  it(`should work`, () => {
-    expect(result).toEqual(expected);
-  });
-});
+const testCases = [
+  {
+    "file_name": "std_func",
+    "description": "simple function generation"
+  },
+  {
+    "file_name": "std_type_method",
+    "description": "standard type method generation"
+  },
+  {
+    "file_name": "std_type_static_method",
+    "description": "standard type static method generation"
+  }
+];
 
-describe("standard type method generation", () => {
-  var metadata = require("./std_type_method.json");
-  var expected = fs.readFileSync("test/std_type_method.out.ts", "utf8");
-  const result = generator(metadata, options);
-  it(`should work`, () => {
-    expect(result).toEqual(expected);
-  });
-});
-
-describe("standard type static method generation", () => {
-  var metadata = require("./std_type_static_method.json");
-  var expected = fs.readFileSync("test/std_type_static_method.out.ts", "utf8");
-  const result = generator(metadata, options);
-  it(`should work`, () => {
-    expect(result).toEqual(expected);
-  });
+testCases.forEach((testCase) => {
+  describe(testCase.description, () => {
+    const metadata = require(`./${testCase.file_name}.json`);
+    const expected = fs.readFileSync(`test/${testCase.file_name}.out.ts`, "utf8");
+    const result = generator(metadata, options);
+    it(`should work`, () => {
+      expect(result).toEqual(expected);
+    });
+  })
 });
