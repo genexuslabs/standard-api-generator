6baa861231305e08ff349c8012826fb80cb8b69c
Marcos Crispino <mcrispino@genexus.com>
2019-03-21 11:01:08 -0300

    Added tests for properties

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -26,6 +26,18 @@ const testCases = [
   {
     "file_name": "implemented_class",
     "description": "class already marked as implemented"
+  },
+  {
+    "file_name": "std_type_property",
+    "description": "standard type property"
+  },
+  {
+    "file_name": "std_type_static_property",
+    "description": "standard type static property"
+  },
+  {
+    "file_name": "std_type_readonly_property",
+    "description": "standard type redonly properties (static & instance)"
   }
 ];
 
